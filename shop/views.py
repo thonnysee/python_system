c9644ea88b2937d5328c075a58b865e80859a8a2
Tony <tony@forwardcompany.com>
2019-11-15 15:26:29 -0600

    Saving project for new dockerfile changes

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Product, Category
 
 # Create your views here.
+def home(request):
+    return render(request,'home.html',context=None)
+
+def shop(request):
+    products = Product.objects.all()
+    categories = Category.objects.order_by('name')
+    data = {
+        'products':products,
+        'categories':categories
+    }
+    return render(request,'shop.html',context=data)
