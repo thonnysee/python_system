8b213518cc532d99fa65b44315b89f040a877d45
Antonio Carbajal <44601648+thonnysee@users.noreply.github.com>
2019-11-22 16:31:59 -0600

    Update views.py
    Removed unused comments and unnecessary print to checkup context

--- shop/views.py
+++ shop/views.py
@@ -12,10 +12,7 @@ class ProductsListView(ListView):
     context_object_name = 'products'
     
     def get_context_data(self, **kwargs):
-        # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
-        # Add in a QuerySet of all the books
         context['categories'] = Category.objects.all()
-        print(context)
         return context
 
