3ca79c100cb6696de2eaca6fa67d73d5e97d53ca
Tony <tony@forwardcompany.com>
2019-11-27 23:57:25 -0600

    Changed models.py of shop to standar pep

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,4 @@
-""" 
+"""
 shop.models.py
 ====================================
 The models of the shop app
@@ -22,18 +22,18 @@ class Category(ParanoidModel):
     description = models.CharField(max_length=264, null=True)
     is_active = models.BooleanField(default=True)
     slug = models.SlugField(unique=True, blank=True, null=True)
-    
+
     class Meta:
         """Adds order by name to model"""
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
         ordering = ["-name"]
-        
+
     def save(self, *args, **kwargs):
         """Adds a slug of the category based on the name"""
         self.slug = slugify(self.name)
         super(Category, self).save(*args, **kwargs)
-    
+
     def __str__(self):
         """Return Category name"""
         return self.name
@@ -43,6 +43,8 @@ class Product(ParanoidModel):
     """
     Stores products with stock control, related to :model:`shop.Category`.
     """
+    u = 'https://aliceasmartialarts.com/wp-content/uploads/2017/04/ \
+        + default-image.jpg'
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50, unique=True)
     summary = models.CharField(max_length=264, null=True)
@@ -53,64 +55,77 @@ class Product(ParanoidModel):
     product_type = models.CharField(max_length=50, null=True)
     slug = models.SlugField(unique=True, blank=True, null=True)
     stock = models.PositiveIntegerField(default=1)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True,related_name='category')
-    url = models.URLField(max_length=200,default="https://aliceasmartialarts.com/wp-content/uploads/2017/04/default-image.jpg")
-    is_active = models.BooleanField(default=True) 
-    
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,
+                                 null=True, related_name='category')
+    url = models.URLField(max_length=200, default=u)
+    is_active = models.BooleanField(default=True)
+
     @property
     def is_active(self):
         return self.is_active
-    
+
     @property
     def has_stock(self):
         return self.stock > 0
-        
+
     def save(self, *args, **kwargs):
         """Adds slug of the Product name"""
         self.slug = slugify(self.name)
         super(Product, self).save(*args, **kwargs)
-    
+
     def __str__(self):
         """Return Product name"""
         return self.sku
 
+
 class Order(ParanoidModel):
-    """Creates the purchase order/invoice of the Customer, related to :model:`auth.User`."""
+    """Creates the purchase order/invoice of the Customer,
+    related to :model:`auth.User`."""
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     order_number = models.PositiveIntegerField(null=False)
     total = models.FloatField(default=0.00)
-    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE, null=True)
     status = models.CharField(max_length=50, default="pending")
-    
-    
+
+
 class OrderItem(ParanoidModel):
     """
     Stores the relation many to many of Products and Orders,
     related to :model:`shop.Products`, :model:`shop.Order`.
     """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    order_id = models.ForeignKey(Order, on_delete=models.CASCADE, null=True,related_name='order')
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, related_name='product')
+    order_id = models.ForeignKey(Order, on_delete=models.CASCADE,
+                                 null=True, related_name='order')
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE,
+                                   null=True, related_name='product')
     unit_price = models.FloatField(default=0)
     quantity = models.PositiveIntegerField(default=1)
-    
+
+
 class Cart(ParanoidModel):
     """Stores the relation cart and customer related to :model:`auth.User`."""
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True,related_name='user')
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE, null=True,
+                                related_name='user')
     state = models.CharField(max_length=50, default='active')
-    
+
+
 class CartItem(ParanoidModel):
     """
-    Stores the relation many to many of Products and Carts if stock is available
-    (Product.stock >= 1), related to :model:`shop.Products`, :model:`shop.Carts`.
+    Stores the relation many to many of Products and Carts if stock
+    is available (Product.stock >= 1), related to :model:`shop.Products`,
+    :model:`shop.Carts`.
     """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     product_id = models.OneToOneField(Product, on_delete=models.CASCADE)
-    cart_id = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='cart')    
+    cart_id = models.ForeignKey(Cart, on_delete=models.CASCADE,
+                                related_name='cart')
     quantity = models.PositiveIntegerField(default=1)
     price = models.FloatField(default=0)
-    
+
+
 class Customer(ParanoidModel):
     """Stores the customer info (TBD)."""
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
