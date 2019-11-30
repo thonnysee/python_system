e8d6c37550fd8212b573c0c3c65840123c8beb20
Tony <tony@forwardcompany.com>
2019-11-29 19:26:06 -0600

    some changes to the tests

--- api/tests/test_user_api.py
+++ api/tests/test_user_api.py
@@ -41,14 +41,17 @@ class PublicUserApiTests(TestCase):
         payload = {'email': 'test@test.com', 'password': 'testpass'}
         create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
-
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_password_too_short(self):
-        """Test that password must be more than 5 characters"""
-        payload = {'email': 'test@test.com', 'password': 'pw'}
+    def test_password_too_common_and_short(self):
+        """Test that password must be not common and not too short"""
+        payload = {
+            'email': 'test@test.com',
+            'password1': 'test',
+            'password2': 'test',
+            'firstname': 'name'
+            }
         res = self.client.post(CREATE_USER_URL, payload)
-
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
             email=payload['email']
